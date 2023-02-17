38cd8321ac6e6250a9888f677cd04b61bf475b88
Martyna Zając <martyna.sabina.zajac@gmail.com>
2023-02-17 19:18:25 +0100

    printed shopping list

--- lista_zakupów.py
+++ lista_zakupów.py
@@ -1,3 +1,6 @@
 #Zadanie 1 Moduł 3
 lista_zakupów={'piekarnia':['chleb', 'pączek', 'bułki'],'warzywniak':['marchew', 'seler', 'rukola']}
 print('Lista zakupów')
+for keys, values in lista_zakupów.items():
+    print(f'Idę do {keys.capitalize()}, kupuję tu następujące rzeczy: {[i.capitalize() for i in values]}')
+
