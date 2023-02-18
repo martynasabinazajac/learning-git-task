adc7edd47a4499bab72c0c161b06babed4b934ca
Martyna Zając <martyna.sabina.zajac@gmail.com>
2023-02-18 21:54:47 +0100

    Add valuest to dictionary

--- lista_zakupów.py
+++ lista_zakupów.py
@@ -1,6 +1,9 @@
 #Zadanie 1 Moduł 3
 lista_zakupów={'piekarnia':['chleb', 'pączek', 'bułki'],'warzywniak':['marchew', 'seler', 'rukola']}
 print('Lista zakupów')
+lista_zakupów['piekarnia'].append('rogal')
+lista_zakupów['warzywniak'].append('burak')
+print(lista_zakupów)
 x=0
 for keys, values in lista_zakupów.items():
     print(f'Idę do {keys.capitalize()}, kupuję tu następujące rzeczy: {[i.capitalize() for i in values]}')
